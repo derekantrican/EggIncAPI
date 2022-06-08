9112127285e94a05f50b87c82dd8f57a1d6b3221
Derek Antrican <derekantrican@gmail.com>
2022-06-07 22:07:06 -0700

    Updated csharp examples

---
+++ csharp/EggIncApi.cs
@@ -0,0 +1,253 @@
+using Ei;
+using Google.Protobuf;
+using static Ei.Backup.Types;
+
+namespace EggIncApi;
+public class EggIncApi
+{
+    private const int CLIENTVERSION = 40;
+    public static async Task<ContractCoopStatusResponse> GetCoopStatus(string contractId, string coopId)
+    {
+        ContractCoopStatusRequest coopStatusRequest = new ContractCoopStatusRequest();
+        coopStatusRequest.ContractIdentifier = contractId;
+        coopStatusRequest.CoopIdentifier = coopId;
+
+        return await MakeEggIncApiRequest("coop_status", coopStatusRequest, ContractCoopStatusResponse.Parser.ParseFrom);
+    }
+
+    public static async Task<EggIncFirstContactResponse> GetFirstContact(string userId)
+    {
+        EggIncFirstContactRequest firstContactRequest = new EggIncFirstContactRequest();
+        firstContactRequest.EiUserId = userId;
+        firstContactRequest.ClientVersion = CLIENTVERSION;
+
+        return await MakeEggIncApiRequest("first_contact", firstContactRequest, EggIncFirstContactResponse.Parser.ParseFrom);
+    }
+
+    public static async Task<PeriodicalsResponse> GetPeriodicals(string userId)
+    {
+        GetPeriodicalsRequest getPeriodicalsRequest = new GetPeriodicalsRequest();
+        getPeriodicalsRequest.UserId = userId;
+        getPeriodicalsRequest.CurrentClientVersion = CLIENTVERSION;
+
+        return await MakeEggIncApiRequest("get_periodicals", getPeriodicalsRequest, PeriodicalsResponse.Parser.ParseFrom);
+    }
+
+    private static async Task<T> MakeEggIncApiRequest<T>(string endpoint, IMessage data, Func<ByteString, T> parseMethod)
+    {
+        byte[] bytes;
+        using (var stream = new MemoryStream())
+        {
+            data.WriteTo(stream);
+            bytes = stream.ToArray();
+        }
+
+        string baseUrl = "https://wasmegg.zw.workers.dev/?url="; 
+        string response = await PostRequest($"{baseUrl}https://www.auxbrain.com/ei/{endpoint}", new FormUrlEncodedContent(new Dictionary<string, string>
+        {
+            { "data", Convert.ToBase64String(bytes) }
+        }));
+
+        //Todo: I don't know if we have to check whether the response is an AuthenticatedMessage or not
+        AuthenticatedMessage authenticatedMessage = AuthenticatedMessage.Parser.ParseFrom(Convert.FromBase64String(response));
+
+        return parseMethod(authenticatedMessage.Message);
+    }
+
+    public static async Task<bool> IsCoopContractOnTrack(LocalContract contract)
+    {
+        var coopStatusResponse = await EggIncApi.GetCoopStatus(contract.Contract.Identifier, contract.CoopIdentifier);
+        var totalEggsPerHour = coopStatusResponse.Contributors.Sum(c => c.ContributionRate * 3600);
+        var targetAmount = contract.Contract.Goals.Last().TargetAmount;
+        var requiredEggsPerHour = ((targetAmount - coopStatusResponse.TotalAmount) / coopStatusResponse.SecondsRemaining) * 3600;
+
+        return totalEggsPerHour >= requiredEggsPerHour;
+    }
+
+    public static bool IsSoloContractOnTrack(LocalContract contract, Backup backup)
+    {
+        //For a solo contract, we cannot simply get the "contribution_rate" of the player (because we don't have coop_status_response).
+        //So we have to calculate manually using researches, artifacts, etc
+
+        int farmIndex = -1;
+        Simulation contractFarm = null;
+        for (int i = 0; i < backup.Farms.Count; i++)
+        {
+            if (backup.Farms[i].ContractId == contract.Contract.Identifier)
+            {
+                contractFarm = backup.Farms[i];
+                farmIndex = i;
+                break;
+            }
+        }
+
+        //ARTIFACTS
+        Dictionary<ArtifactSpec.Types.Name, double[][]> artifactMultipliers = new Dictionary<ArtifactSpec.Types.Name, double[][]>
+        {
+            { 
+                ArtifactSpec.Types.Name.QuantumMetronome, 
+                new[]
+                {
+                    new[] { 0.05 }, //Misaligned
+                    new[] { 0.1, 0.12 }, //Adequate
+                    new[] { 0.15, 0.17, 0.2 }, //Perfect
+                    new[] { 0.25, 0.27, 0.3, 0.35 }, //Reggference
+                }
+            },
+            { 
+                ArtifactSpec.Types.Name.InterstellarCompass, 
+                new[]
+                {
+                    new[] { 0.05 }, //Miscalibrated
+                    new[] { 0.1 }, //Regular
+                    new[] { 0.2, 0.22 }, //Precise
+                    new[] { 0.3, 0.35, 0.4, 0.5 }, //Clairvoyant
+                }
+            },
+            { 
+                ArtifactSpec.Types.Name.TachyonStone, 
+                new[]
+                {
+                    //Fragment cannot be set (no level)
+                    new[] { 0.02 }, //Regular
+                    new[] { 0.04 }, //Eggsquisite
+                    new[] { 0.05 }, //Brilliant
+                }
+            },
+            { 
+                ArtifactSpec.Types.Name.QuantumStone, 
+                new[]
+                {
+                    //Fragment cannot be set (no level)
+                    new[] { 0.02 }, //Regular
+                    new[] { 0.04 }, //Phased
+                    new[] { 0.05 }, //Meggnificient
+                }
+            },
+        };
+
+        var artifactInventory = backup.ArtifactsDb.InventoryItems;
+        var contractFarmArtifactSlots = backup.ArtifactsDb.ActiveArtifactSets[farmIndex].Slots;
+
+        double eggLayingRateArtifactMultiplier = 1;
+        double shippingRateArtifactMultiplier = 1;
+        foreach (var artifactSlot in contractFarmArtifactSlots)
+        {
+            if (artifactSlot.Occupied)
+            {
+                var artifact = artifactInventory.FirstOrDefault(a => a.ItemId == artifactSlot.ItemId);
+                if (artifact.Artifact.Spec.Name == ArtifactSpec.Types.Name.QuantumMetronome)
+                {
+                    var artifactMultiplier = artifactMultipliers[artifact.Artifact.Spec.Name][(int)artifact.Artifact.Spec.Level][(int)artifact.Artifact.Spec.Rarity];
+                    eggLayingRateArtifactMultiplier *= 1 + artifactMultiplier;
+                }
+                else if (artifact.Artifact.Spec.Name == ArtifactSpec.Types.Name.QuantumMetronome)
+                {
+                    var artifactMultiplier = artifactMultipliers[artifact.Artifact.Spec.Name][(int)artifact.Artifact.Spec.Level][(int)artifact.Artifact.Spec.Rarity];
+                    shippingRateArtifactMultiplier *= 1 + artifactMultiplier;
+                }
+
+                foreach (var stone in artifact.Artifact.Stones)
+                {
+                    if (stone.Name == ArtifactSpec.Types.Name.TachyonStone)
+                    {
+                        var stoneMultiplier = artifactMultipliers[artifact.Artifact.Spec.Name][(int)artifact.Artifact.Spec.Level][(int)artifact.Artifact.Spec.Rarity];
+                        eggLayingRateArtifactMultiplier *= 1 + stoneMultiplier;
+                    }
+                    else if (stone.Name == ArtifactSpec.Types.Name.QuantumStone)
+                    {
+                        var stoneMultiplier = artifactMultipliers[artifact.Artifact.Spec.Name][(int)artifact.Artifact.Spec.Level][(int)artifact.Artifact.Spec.Rarity];
+                        shippingRateArtifactMultiplier *= 1 + stoneMultiplier;
+                    }
+                }
+            }
+        }
+
+        //EGG LAYING RATE
+        Dictionary<string, double> eggLayingResearchMultipliersPerLevel = new Dictionary<string, double>
+        {
+            { "comfy_nests", 0.1 },
+            { "hen_house_ac", 0.05 },
+            { "improved_genetics", 0.15 },
+            { "time_compress", 0.1 },
+            { "timeline_diversion", 0.02 },
+            { "relativity_optimization", 0.1 },
+            { "epic_egg_laying", 0.05 },
+        };
+
+        double eggLayingRateResearchMultiplier = 1;
+        foreach (var commonResearch in contractFarm.CommonResearch)
+        {
+            if (eggLayingResearchMultipliersPerLevel.ContainsKey(commonResearch.Id))
+            {
+                eggLayingRateResearchMultiplier *= 1 + eggLayingResearchMultipliersPerLevel[commonResearch.Id] * commonResearch.Level;
+            }
+        }
+
+        var epicComfyNestsLevel = backup.Game.EpicResearch.FirstOrDefault(er => er.Id == "epic_egg_laying").Level;
+        eggLayingRateResearchMultiplier *= 1 + eggLayingResearchMultipliersPerLevel["epic_egg_laying"] * epicComfyNestsLevel;
+
+        var layableEggsPerSecond = contractFarm.NumChickens * 1/30 * eggLayingRateResearchMultiplier * eggLayingRateArtifactMultiplier;
+
+        //SHIPPING CAPACITY
+        var vehicleCapacities = new List<double>();
+        for (int i = 0; i < contractFarm.Vehicles.Count; i++)
+        {
+            if (contractFarm.Vehicles[i] != 11) //Calculation is based off all vehicles being hyperloops
+            {
+                Console.WriteLine("  Found vehicle that is not a hyperloop. These calculations are all based on hyperloops so these calculations will not be accurate...");
+            }
+            else
+            {
+                vehicleCapacities.Add((50e6/60) * contractFarm.TrainLength[i]);
+            }
+        }
+
+        Dictionary<string, double> shippingRateResearchMultipliersPerLevel = new Dictionary<string, double>
+        {
+            { "leafsprings" , 0.05 },
+            { "lightweight_boxes" , 0.1 },
+            { "driver_training" , 0.05 },
+            { "super_alloy" , 0.05 },
+            { "quantum_storage" , 0.05 },
+            { "hover_upgrades" , 0.05 }, //Only valid for hover vehicles (but our calculations assume that all vehicles are hyperloops)
+            { "dark_containment" , 0.05 },
+            { "neural_net_refine" , 0.05 },
+            { "hyper_portalling" , 0.05 }, //Only valid for hyperloops (but our calculations assume that all vehicles are hyperloops)
+            { "transportation_lobbyist" , 0.05 },
+        };
+
+        double shippingRateResearchMultiplier = 1;
+        foreach (var commonResearch in contractFarm.CommonResearch)
+        {
+            if (shippingRateResearchMultipliersPerLevel.ContainsKey(commonResearch.Id))
+            {
+                shippingRateResearchMultiplier *= 1 + shippingRateResearchMultipliersPerLevel[commonResearch.Id] * commonResearch.Level;
+            }
+        }
+
+        var transportationLobbyistLevel = backup.Game.EpicResearch.FirstOrDefault(er => er.Id == "transportation_lobbyist").Level;
+        shippingRateResearchMultiplier *= 1 + shippingRateResearchMultipliersPerLevel["transportation_lobbyist"] * transportationLobbyistLevel;
+
+        var shippableEggsPerSecond = vehicleCapacities.Sum(v => v * shippingRateResearchMultiplier * shippingRateArtifactMultiplier);
+
+        //FINAL CALCULATION
+        var targetAmount = contract.Contract.Goals.Last().TargetAmount;
+        var contractEndTime = contract.TimeAccepted + contract.Contract.LengthSeconds;
+        var secondsRemaining = contractEndTime - new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+
+        var requiredEggsPerHour = ((targetAmount - contractFarm.EggsLaid) / secondsRemaining) * 3600;
+        var eggsPerHour = Math.Min(layableEggsPerSecond, shippableEggsPerSecond) * 3600;
+
+        return eggsPerHour >= requiredEggsPerHour;
+    }
+
+    private static async Task<string> PostRequest(string url, FormUrlEncodedContent json)
+    {
+        using (var client = new HttpClient())
+        {
+            var response = await client.PostAsync(url, json);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,72 +1,29 @@
-﻿using System.Net;
-using System.Net.Http.Json;
-using System.Text;
-using System.Text.Json;
-using Ei;
-using Google.Protobuf;
+﻿var coopStatus = await EggIncApi.EggIncApi.GetCoopStatus("shipping-surge", "shippingrun");
 
-await GetCoopStatus("shipping-surge", "shippingrun");
+// await GetCurrentContractsForUserWithOnTrack("EI1234567890123456");
 
-async Task GetCurrentCoopsForUser(string userId)
+async Task GetCurrentContractsForUserWithOnTrack(string userId)
 {
-    EggIncFirstContactRequest firstContactRequest = new EggIncFirstContactRequest();
-    firstContactRequest.EiUserId = userId;
-    firstContactRequest.ClientVersion = 36;
-
-    byte[] bytes;
-    using (var stream = new MemoryStream())
-    {
-        firstContactRequest.WriteTo(stream);
-        bytes = stream.ToArray();
-    }
-
-    string response = await PostRequest("https://wasmegg.zw.workers.dev/?url=https://www.auxbrain.com/ei/first_contact", new FormUrlEncodedContent(new Dictionary<string, string>
-    {
-        { "data", Convert.ToBase64String(bytes) }
-    }));
-
-    AuthenticatedMessage authenticatedMessage = AuthenticatedMessage.Parser.ParseFrom(Convert.FromBase64String(response));
-
-    EggIncFirstContactResponse firstContactResponse = EggIncFirstContactResponse.Parser.ParseFrom(authenticatedMessage.Message);
-    foreach (var contract in firstContactResponse.Backup.Contracts.Contracts)
+    var firstContact = await EggIncApi.EggIncApi.GetFirstContact(userId);
+    foreach (var contract in firstContact.Backup.Contracts.Contracts)
     {
         Console.WriteLine("current coop details: ");
         Console.WriteLine($"  contract_id: {contract.Contract.Identifier}");
         Console.WriteLine($"  coop_id: {contract.CoopIdentifier}");
-    }
-}
 
-async Task GetCoopStatus(string contractId, string coopId)
-{
-    ContractCoopStatusRequest coopStatusRequest = new ContractCoopStatusRequest();
-    coopStatusRequest.ContractIdentifier = contractId;
-    coopStatusRequest.CoopIdentifier = coopId;
-
-    byte[] bytes;
-    using (var stream = new MemoryStream())
-    {
-        coopStatusRequest.WriteTo(stream);
-        bytes = stream.ToArray();
-    }
-
-    string url = "https://wasmegg.zw.workers.dev/?url=https://www.auxbrain.com/ei/coop_status";
-    string response = await PostRequest(url, new FormUrlEncodedContent(new Dictionary<string, string>
-    {
-        { "data", Convert.ToBase64String(bytes) }
-    }));
-
-    AuthenticatedMessage authenticatedMessage = AuthenticatedMessage.Parser.ParseFrom(Convert.FromBase64String(response));
-
-    ContractCoopStatusResponse contractCoopStatusResponse = ContractCoopStatusResponse.Parser.ParseFrom(authenticatedMessage.Message);
-
-    Console.WriteLine(JsonSerializer.Serialize(contractCoopStatusResponse));
-}
-
-async Task<string> PostRequest(string url, FormUrlEncodedContent json)
-{
-    using (var client = new HttpClient())
-    {
-        var response = await client.PostAsync(url, json);
-        return await response.Content.ReadAsStringAsync();
+        if (contract.Contract.CoopAllowed)
+        {
+            if (await EggIncApi.EggIncApi.IsCoopContractOnTrack(contract))
+            {
+                Console.WriteLine("  contract is on track!");
+            }
+        }
+        else
+        {
+            if (EggIncApi.EggIncApi.IsSoloContractOnTrack(contract, firstContact.Backup))
+            {
+                Console.WriteLine("  contract is on track!");
+            }
+        }
     }
 }
