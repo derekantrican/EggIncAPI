f993e133e5d4c991a9ce079cb674dd8a0de27e06
Derek Antrican <derekantrican@gmail.com>
2022-06-09 22:26:35 -0700

    Updated python examples to account for change to first_contact endpoint

--- python/getCurrentCoopsForUser.py
+++ python/getCurrentCoopsForUser.py
@@ -1,3 +1,5 @@
+import binascii
+import hashlib
 import requests
 import ei_pb2
 import base64
@@ -5,12 +7,27 @@ from google.protobuf.json_format import MessageToJson
 
 user_id = 'EI1234567890123456'
 
+def sha256(bytes_obj):
+    h = hashlib.sha256()
+    h.update(bytes_obj)
+    return binascii.hexlify(h.digest()) 
+
+def hashexample(message_bytes):
+    data = bytearray(message_bytes)
+    data[0x3b9af419 % len(data)] = 0x1b
+    data.extend(sha256(b'THE SECRETS OF THE UNIVERSE WILL BE UNLOCKED'))
+    return sha256(data).decode('ascii')
+
 first_contact_request = ei_pb2.EggIncFirstContactRequest()
 first_contact_request.ei_user_id = user_id
 first_contact_request.client_version = 36
 
-url = 'https://wasmegg.zw.workers.dev/?url=https://www.auxbrain.com/ei/first_contact'
-data = { 'data' : base64.b64encode(first_contact_request.SerializeToString()).decode('utf-8') }
+authenticated_request = ei_pb2.AuthenticatedMessage()
+authenticated_request.code = hashexample(first_contact_request.SerializeToString())
+authenticated_request.message = first_contact_request.SerializeToString()
+
+url = 'https://wasmegg.zw.workers.dev/?url=https://www.auxbrain.com/ei/first_contact_secure'
+data = { 'data' : base64.b64encode(authenticated_request.SerializeToString()).decode('utf-8') }
 response = requests.post(url, data = data)
 
 authenticated_message = ei_pb2.AuthenticatedMessage()
