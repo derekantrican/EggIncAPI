0dbe6c6520958c09b3e1328844a66bd9314b4f46
Derek Antrican <deantric@microsoft.com>
2022-09-26 21:25:48 -0700

    Fixed a bug and added a couple helper methods to EggIncApi.cs

--- csharp/EggIncApi.cs
+++ csharp/EggIncApi.cs
@@ -156,12 +156,12 @@ public class EggIncApi
                 {
                     if (stone.Name == ArtifactSpec.Types.Name.TachyonStone)
                     {
-                        var stoneMultiplier = artifactMultipliers[artifact.Artifact.Spec.Name][(int)artifact.Artifact.Spec.Level][(int)artifact.Artifact.Spec.Rarity];
+                        var stoneMultiplier = artifactMultipliers[stone.Name][(int)stone.Level][0];
                         eggLayingRateArtifactMultiplier *= 1 + stoneMultiplier;
                     }
                     else if (stone.Name == ArtifactSpec.Types.Name.QuantumStone)
                     {
-                        var stoneMultiplier = artifactMultipliers[artifact.Artifact.Spec.Name][(int)artifact.Artifact.Spec.Level][(int)artifact.Artifact.Spec.Rarity];
+                        var stoneMultiplier = artifactMultipliers[stone.Name][(int)stone.Level][0];
                         shippingRateArtifactMultiplier *= 1 + stoneMultiplier;
                     }
                 }
@@ -247,6 +247,140 @@ public class EggIncApi
         return eggsPerHour >= requiredEggsPerHour;
     }
 
+    public static double CalcuateOfflineInternalHatcheryRate(Simulation farm, Backup backup)
+    {
+        //ARTIFACTS
+        Dictionary<ArtifactSpec.Types.Name, double[][]> artifactMultipliers = new Dictionary<ArtifactSpec.Types.Name, double[][]>
+        {
+            { 
+                ArtifactSpec.Types.Name.TheChalice, 
+                new[]
+                {
+                    new[] { 0.05 }, //Plain
+                    new[] { 0.1, 0.0, 0.15 }, //Polished (note: according to the wiki, "rare" apparently isn't possible)
+                    new[] { 0.2, 0.23, 0.25 }, //Jeweled
+                    new[] { 0.30, 0.0, 0.35, 0.4 }, //Eggceptional (note: according to the wiki, "rare" apparently isn't possible)
+                }
+            },
+            { 
+                ArtifactSpec.Types.Name.LifeStone, 
+                new[]
+                {
+                    //Fragment cannot be set (no level)
+                    new[] { 0.02 }, //Regular
+                    new[] { 0.03 }, //Good
+                    new[] { 0.04 }, //Eggceptional
+                }
+            },
+            { 
+                ArtifactSpec.Types.Name.ClarityStone, 
+                new[]
+                {
+                    //Fragment cannot be set (no level)
+                    new[] { 0.25 }, //Regular
+                    new[] { 0.5 }, //Eggsquisite
+                    new[] { 1.0 }, //Eggceptional
+                }
+            },
+        };
+
+        var artifactInventory = backup.ArtifactsDb.InventoryItems;
+        var contractFarmArtifactSlots = backup.ArtifactsDb.ActiveArtifactSets[0].Slots;
+
+        double internalHatcheryRateArtifactMultiplier = 1;
+        foreach (var artifactSlot in contractFarmArtifactSlots)
+        {
+            if (artifactSlot.Occupied)
+            {
+                var artifact = artifactInventory.FirstOrDefault(a => a.ItemId == artifactSlot.ItemId);
+                if (artifact.Artifact.Spec.Name == ArtifactSpec.Types.Name.TheChalice)
+                {
+                    var artifactMultiplier = artifactMultipliers[artifact.Artifact.Spec.Name][(int)artifact.Artifact.Spec.Level][(int)artifact.Artifact.Spec.Rarity];
+
+                    if (farm.EggType == Ei.Egg.Enlightenment)
+                    {
+                        double clarityStoneMultiplier = 0;
+                        foreach (var stone in artifact.Artifact.Stones)
+                        {
+                            if (stone.Name == ArtifactSpec.Types.Name.ClarityStone)
+                            {
+                                clarityStoneMultiplier += artifactMultipliers[stone.Name][(int)stone.Level][0];
+                            }
+                        }
+
+                        artifactMultiplier *= clarityStoneMultiplier;
+                    }
+
+                    internalHatcheryRateArtifactMultiplier *= 1 + artifactMultiplier;
+                }
+
+                foreach (var stone in artifact.Artifact.Stones)
+                {
+                    if (stone.Name == ArtifactSpec.Types.Name.LifeStone)
+                    {
+                        var stoneMultiplier = artifactMultipliers[stone.Name][(int)stone.Level][0];
+                        internalHatcheryRateArtifactMultiplier *= 1 + stoneMultiplier;
+                    }
+                }
+            }
+        }
+
+        //EGG LAYING RATE
+        Dictionary<string, double> internalHatcheryResearchMultipliersPerLevel = new Dictionary<string, double>
+        {
+            { "internal_hatchery1", 2 },
+            { "internal_hatchery2", 5 },
+            { "internal_hatchery3", 10 },
+            { "internal_hatchery4", 25 },
+            { "internal_hatchery5", 5 },
+            { "neural_linking", 50 },
+            { "epic_internal_incubators", 0.05 },
+            { "int_hatch_calm", 0.1 }
+            //Assumed that the epic research "Internal Hatchery Sharing" does not come into play (this is what wasmegg does)
+        };
+
+        double internalHatcheryRateResearchMultiplier = 0;
+        foreach (var commonResearch in farm.CommonResearch)
+        {
+            if (internalHatcheryResearchMultipliersPerLevel.ContainsKey(commonResearch.Id))
+            {
+                //Common researches for IHR are additive
+                internalHatcheryRateResearchMultiplier += internalHatcheryResearchMultipliersPerLevel[commonResearch.Id] * commonResearch.Level;
+            }
+        }
+
+        //Epic researches for IHR are compounding
+        var epicInternalHatcheriesLevel = backup.Game.EpicResearch.FirstOrDefault(er => er.Id == "epic_internal_incubators").Level;
+        internalHatcheryRateResearchMultiplier *= 1 + internalHatcheryResearchMultipliersPerLevel["epic_internal_incubators"] * epicInternalHatcheriesLevel;
+
+        var internalHatcheryCalmLevel = backup.Game.EpicResearch.FirstOrDefault(er => er.Id == "int_hatch_calm").Level;
+        internalHatcheryRateResearchMultiplier *= 1 + internalHatcheryResearchMultipliersPerLevel["int_hatch_calm"] * internalHatcheryCalmLevel;
+
+        return internalHatcheryRateArtifactMultiplier * internalHatcheryRateResearchMultiplier * 4; //Assume 4 habs (this is what wasmegg does)
+    }
+
+    public static double CalculateHabSpace(Simulation farm)
+    {
+        Dictionary<string, double> habCapacityResearchMultipliersPerLevel = new Dictionary<string, double>
+        {
+            { "hab_capacity1", 0.05 },
+            { "microlux", 0.05 },
+            { "grav_plating", 0.02 },
+            { "wormhole_dampening", 0.02 },
+        };
+
+        double habCapacityResearchMultiplier = 1;
+        foreach (var commonResearch in farm.CommonResearch)
+        {
+            if (habCapacityResearchMultipliersPerLevel.ContainsKey(commonResearch.Id))
+            {
+                habCapacityResearchMultiplier *= 1 + habCapacityResearchMultipliersPerLevel[commonResearch.Id] * commonResearch.Level;
+            }
+        }
+
+        return Math.Round(habCapacityResearchMultiplier * 4 * 6e8); //Assumes 4 habs and all Chicken Universes
+    }
+
     private static async Task<string> PostRequest(string url, FormUrlEncodedContent json)
     {
         using (var client = new HttpClient())
@@ -254,5 +388,66 @@ public class EggIncApi
             var response = await client.PostAsync(url, json);
             return await response.Content.ReadAsStringAsync();
         }
+    }
+
+    private static Dictionary<int, string> roles = new Dictionary<int, string>
+    {
+        { 0, "Farmer" },
+        { 1, "Farmer II" },
+        { 2, "Farmer III" },
+        { 3, "Kilofarmer" },
+        { 4, "Kilofarmer II" },
+        { 5, "Kilofarmer III" },
+        { 6, "Megafarmer" },
+        { 7, "Megafarmer II" },
+        { 8, "Megafarmer III" },
+        { 9, "Gigafarmer" },
+        { 10, "Gigafarmer II" },
+        { 11, "Gigafarmer III" },
+        { 12, "Terafarmer" },
+        { 13, "Terafarmer II" },
+        { 14, "Terafarmer III" },
+        { 15, "Petafarmer" },
+        { 16, "Petafarmer II" },
+        { 17, "Petafarmer III" },
+        { 18, "Exafarmer" },
+        { 19, "Exafarmer II" },
+        { 20, "Exafarmer III" },
+        { 21, "Zettafarmer" },
+        { 22, "Zettafarmer II" },
+        { 23, "Zettafarmer III" },
+        { 24, "Yottafarmer" },
+        { 25, "Yottafarmer II" },
+        { 26, "Yottafarmer III" },
+        { 27, "Xennafarmer" },
+        { 28, "Xennafarmer II" },
+        { 29, "Xennafarmer III" },
+        { 30, "Weccafarmer" },
+        { 31, "Weccafarmer II" },
+        { 32, "Weccafarmer III" },
+        { 33, "Vendafarmer" },
+        { 34, "Vendafarmer II" },
+        { 35, "Vendafarmer III" },
+        { 36, "Uadafarmer" },
+        { 37, "Uadafarmer II" },
+        { 38, "Uadafarmer III" },
+        { 39, "Treidafarmer" },
+        { 40, "Treidafarmer II" },
+        { 41, "Treidafarmer III" },
+        { 42, "Quadafarmer" },
+        { 43, "Quadafarmer II" },
+        { 44, "Quadafarmer III" },
+        { 45, "Pendafarmer" },
+        { 46, "Pendafarmer II" },
+        { 47, "Pendafarmer III" },
+        { 48, "Exedafarmer" },
+        { 49, "Exedafarmer II" },
+        { 50, "Exedafarmer III" },
+        { 51, "Infinifarmer" },
+    };
+
+    public static string SoulPowerToFarmerRole(double soulPower)
+    {
+        return roles[(int)Math.Floor(soulPower)];
     }
 }
